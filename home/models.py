464bb17006c1889a0d4d8735b8924954a285d54c
anderson5692 <blueoystercult1982@gmail.com>
2021-05-07 17:37:43 -0300

    0001_initial

--- home/models.py
+++ home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Personagem(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(max_length=255)
