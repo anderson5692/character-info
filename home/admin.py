06dce205034eeae25ac7d76a9b1d26ccce5e5b7e
anderson5692 <blueoystercult1982@gmail.com>
2021-05-10 17:28:52 -0300

    att to classes

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
-#from.models import
-
 # Register your models here.
+from django.contrib import admin
+from .models import Personagem
+
+admin.site.register(Personagem)
--- home/models.py
+++ home/models.py
@@ -3,4 +3,12 @@ from django.db import models
 # Create your models here.
 class Personagem(models.Model):
     nome = models.CharField(max_length=255)
+    idade = models.IntegerChoices(max_lenght=9999)
+    especie = models.CharField(max_length=255)
+    genero = models.CharField(max_length=255)
+    ocupacao = models.CharField(max_length=255)
     descricao = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
