98ed4c9c0513467b50eaafaf9194290e25cc4624
anderson5692 <blueoystercult1982@gmail.com>
2021-05-11 22:38:57 -0300

    add 1 char

--- home/models.py
+++ home/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Personagem(models.Model):
-    nome = models.CharField(max_length=255)
-    idade = models.IntegerChoices(max_lenght=9999)
-    especie = models.CharField(max_length=255)
-    genero = models.CharField(max_length=255)
-    ocupacao = models.CharField(max_length=255)
-    descricao = models.TextField(max_length=255)
+    Nome = models.CharField(max_length=255)
+    Idade = models.IntegerField(blank=True, null=True)
+    Espécie = models.CharField(max_length=255)
+    Gênero = models.CharField(max_length=255)
+    Ocupação = models.CharField(max_length=255)
+    Descrição = models.TextField(max_length=255)
 
     def __str__(self):
-        return self.nome
+        return self.Nome
 
