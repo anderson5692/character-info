43a1c9f6d6f1f4968ae8b22774e08a36d594c7e3
anderson5692 <blueoystercult1982@gmail.com>
2021-05-27 15:55:33 -0300

    /

--- home/views.py
+++ home/views.py
@@ -1,71 +1,84 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Personagem
 
-def index (request):
+
+def index(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/index.html', {
         'personagens': personagens
     })
 
-def Finn (request):
+
+def Finn(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Jake (request):
+
+def Jake(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Marceline (request):
+
+def Marceline(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Princesacaroco (request):
+
+def Princesacaroco(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Princesajujuba (request):
+
+def Princesajujuba(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Princesadefogo (request):
+
+def Princesadefogo(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def BMO (request):
+
+def BMO(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Ladyiris (request):
+
+def Ladyiris(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Reigelado (request):
+
+def Reigelado(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
 
-def Lich (request):
+
+def Lich(request):
     personagens = Personagem.objects.all()
     return render(request, 'home/Finn.html', {
         'personagens': personagens
     })
+
+
 """
 def info_personagem (request, personagem_Nome):
     # info = Personagem.objects.get(Nome=personagem_Nome)
